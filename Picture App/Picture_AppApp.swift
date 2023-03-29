905ae058cfcaf55463a785f3eabc76d384d48aaf
Marnie Reid <marnie@kodewithklossy.com>
2023-03-29 18:02:53 +0200

    Initial Commit

---
+++ Picture App/Picture_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Picture_AppApp.swift
+//  Picture App
+//
+//  Created by scholar on 3/29/23.
+//
+
+import SwiftUI
+
+@main
+struct Picture_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
