c0557744d7622b4b0bf89721757b41fcbb6f4940
Marnie Reid <marnie@kodewithklossy.com>
2024-03-25 13:45:10 -0400

    Complete Project (including mild/medium/spicy challenges)

--- Picture App/ContentView.swift
+++ Picture App/ContentView.swift
@@ -9,13 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Here's a cute puppy!")
+                .font(.title)
+                .foregroundColor(Color.blue)
+            
+            Image("cute puppy")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+               //Medium Challenge
+                .cornerRadius(10)
+            
+            //Mild Challenge
+            Text("Here's another cute puppy!")
+                .font(.title)
+                .foregroundColor(Color.purple)
+            
+            Image("Cute_dog")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+                //Medium Challenge
+                .cornerRadius(10)
+            
+            //Spicy Challenge
+            Button("Click me!") {
+                //Super Spicy Challenge
+                print("Button tapped!")
+            }
+            .font(.headline)
+            .foregroundColor(Color.mint)
+
+            
         }
         .padding()
+    
     }
 }
 
